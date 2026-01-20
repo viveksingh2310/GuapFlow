c1c08f15bfc1d64afbbf996fd95a3eaa6d50a715
viveksingh2310 <viveksingh14538@gmail.com>
2026-01-20 16:14:48 +0530

    env related changes

--- backend/user-service/app/core/config.py
+++ backend/user-service/app/core/config.py
@@ -7,7 +7,7 @@ class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str= os.getenv("SECRET_KEY")
     ALGORITHM:str=os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES:int=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    ACCESS_TOKEN_EXPIRES_MINUTES:int=os.getenv("ACCESS_TOKEN_EXPIRES_MINUTES")
     ROLE_ADMIN:str=os.getenv("ROLE_ADMIN")
     ROLE_USER:str=os.getenv("ROLE_USER")
     if DATABASE_URL is None:
@@ -16,7 +16,7 @@ class Settings:
         raise RuntimeError("SECRET_KEY is not set in .env file")
     if ALGORITHM is None:
         raise RuntimeError("ALGORITHM is not set in the .env file")
-    if ACCESS_TOKEN_EXPIRE_MINUTES is None:
+    if ACCESS_TOKEN_EXPIRES_MINUTES is None:
         raise RuntimeError("ACCESS_TOKEN_EXPIRES_MINUTES is not set in the .env file")
     if ROLE_ADMIN is None:
         raise RuntimeError("ROLE_ADMIN is not set in .env file")
